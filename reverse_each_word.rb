873ce48db1e782c486310f7bf9dd8d7119008237
Chelsey <cswilik@gmail.com>
2020-10-13 02:29:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,8 @@ def reverse_each_word(sentence)
   array.join(" ")
 end 
  
-
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
 # def reverse_each_word(sentence)
 #   reversed = sentence.split(" ").collect do |word|
 #   word.reverse
